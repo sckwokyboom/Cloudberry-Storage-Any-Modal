71c4ed4812f1ce372b16bb442473ecbe99c2924a
sckwo <sckwokyboom@gmail.com>
2025-04-03 04:36:44 +0700

    Поправлены импорты

--- src/cloudberry_storage.py
+++ src/cloudberry_storage.py
@@ -14,9 +14,9 @@ from qdrant_client import models, QdrantClient
 from qdrant_client.http.exceptions import UnexpectedResponse
 from qdrant_client.models import Distance, VectorParams
 
-import generated.cloudberry_storage_pb2 as pb2
-import generated.cloudberry_storage_pb2_grpc as pb2_grpc
-from generated.cloudberry_storage_pb2 import InitBucketRequest, DestroyBucketRequest, Empty, FindRequest, \
+import src.generated.cloudberry_storage_pb2 as pb2
+import src.generated.cloudberry_storage_pb2_grpc as pb2_grpc
+from src.generated.cloudberry_storage_pb2 import InitBucketRequest, DestroyBucketRequest, Empty, FindRequest, \
     RemoveEntryRequest, PutEntryRequest, ImageEntry, FindResponse, FindResponseEntry
 from src.embedders.one_peace_embedder import OnePeaceMultimodalEmbedder
 from src.embedders.sbert_embedder import SBERTEmbedder
