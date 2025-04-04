c54896ab0fcb2c12ee3c5d38280b538767b26655
sckwo <sckwokyboom@gmail.com>
2025-04-04 20:05:40 +0700

    Объединил proto-файлы

--- scripts/generate_proto.py
+++ scripts/generate_proto.py
@@ -10,14 +10,6 @@ def main():
         "--grpc_python_out=src/generated",
         "cloudberry_storage.proto"
     ], check=True)
-    subprocess.run([
-        "python3", "-m", "grpc_tools.protoc",
-        "-I.",
-        "--python_out=src/generated",
-        "--pyi_out=src/generated",
-        "--grpc_python_out=src/generated",
-        "one_peace_service.proto"
-    ], check=True)
 
 
 if __name__ == "__main__":
