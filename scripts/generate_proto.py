3cda54c3f961b354e7cc0cf9198fb1db7ca85f8b
sckwo <sckwokyboom@gmail.com>
2025-04-05 00:43:48 +0700

    Логически разбил proto-файлы

--- scripts/generate_proto.py
+++ scripts/generate_proto.py
@@ -11,6 +11,15 @@ def main():
         "cloudberry_storage.proto"
     ], check=True)
 
+    subprocess.run([
+        "python3", "-m", "grpc_tools.protoc",
+        "-I.",
+        "--python_out=src/generated",
+        "--pyi_out=src/generated",
+        "--grpc_python_out=src/generated",
+        "one_peace_service.proto"
+    ], check=True)
+
 
 if __name__ == "__main__":
     main()
